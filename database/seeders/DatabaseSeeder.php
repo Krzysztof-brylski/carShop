6527a10fa5885a95aff367fc484498b664f172e2
krzysztof-brylski1 <krzysztof.brylski1@o2.pl>
2023-01-09 17:31:35 +0100

    implement all important seeders in main seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            CarEquipmentSeeder::class,
+            ConfirmationSeeder::class,
+            OfferSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
